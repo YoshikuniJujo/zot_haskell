008cf71e23096fbe3e951fe14afea89e7f9d4f1b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-12 17:56:55 +0900

    refactoring: Zot not yet finished

--- src/Zot.hs
+++ src/Zot.hs
@@ -2,47 +2,51 @@ module Zot ( main, mainFile ) where
 
 import Data.Char ( isSpace )
 
-data Fun = Fun ( Fun -> IO Fun ) | Int Int
+data Fun = Fun ( Fun -> IO Fun ) | Zero | One
 
 instance Show Fun where
-	show ( Int int )	= show int
-	show Fun{ }		= "function"
+	show Zero	= "0"
+	show One	= "1"
+	show ( Fun _ )	= "function"
 
 apply :: Fun -> Fun -> IO Fun
-apply ( Fun f )		= f
-apply ( Int _ )		= apply i
+apply ( Fun f )	= f
+apply Zero	= apply zero
+apply One	= apply one
 
-($$) :: Fun -> Fun -> IO Fun
-f $$ a = apply f a
+( $$ ) :: Fun -> Fun -> IO Fun
+( $$ ) = apply
 
-infixl 7 $$$
+infixl 7 >$$
+( >$$ ) :: IO Fun -> Fun -> IO Fun
+f >$$ a = f >>= ( $$ a )
 
-($$$) :: IO Fun -> Fun -> IO Fun
-f $$$ a = f >>= ( $$ a )
+infixr 8 $$<
+( $$< ) :: Fun -> IO Fun -> IO Fun
+f $$< a = ( f $$ ) =<< a
 
-infixr 8 $$$$
+infixl 6 >$$<
+( >$$< ) :: IO Fun -> IO Fun -> IO Fun
+f >$$< a = f >>= ( $$< a )
 
-($$$$) :: Fun -> IO Fun -> IO Fun
-f $$$$ a = a >>= ( f $$  )
+retFun :: ( Fun -> IO Fun ) -> IO Fun
+retFun = return . Fun
 
 cont :: Fun -> Fun
 cont = Fun . flip ( $$ )
 
 retCont :: Fun -> IO Fun
-retCont = return . Fun . flip ( $$ )
+retCont = return . cont
 
 s, k, i :: Fun
-s = Fun $ \x -> return $ Fun $ \y -> return $ Fun $ \z -> do
-	xz <- x $$ z
-	yz <- y $$ z
-	xz $$ yz
-k = Fun $ return . Fun . const . return
+s = Fun $ \x -> retFun $ \y -> retFun $ \z -> x $$ z >$$< y $$ z
+k = Fun $ retFun . const . return
 i = Fun return
 
 empty, zero, one :: Fun
 empty	= cont i
-zero	= cont $ Fun $ \f -> f $$ s $$$ k
-one	= Fun $ \c -> retCont $	Fun $ \l -> retCont $ Fun $ \r -> c $$$$ ( l $$ r )
+zero	= cont $ Fun $ \f -> f $$ s >$$ k
+one	= Fun $ \c -> retCont $	Fun $ \l -> retCont $ Fun $ \r -> c $$< l $$ r
 
 remCom :: String -> String
 remCom = unlines . map ( takeWhile ( /= '#' ) ) . lines
@@ -50,29 +54,29 @@ remCom = unlines . map ( takeWhile ( /= '#' ) ) . lines
 main :: IO ()
 main = do
 	fun <- getContents >>= makeZot . filter ( not . isSpace ) . remCom
-	fun $$$$ output $$$ pr >> putStrLn ""
+	fun $$< output >$$ pr >> putStrLn ""
 
 mainFile :: String -> IO ()
 mainFile fn = do
 	prg <- readFile fn
 	arg <- getContents
 	fun <- makeZot $ filter ( not . isSpace ) $ remCom $ prg ++ arg
-	fun $$$$ output $$$ pr >> putStrLn ""
+	fun $$< output >$$ pr >> putStrLn ""
 
 makeZot :: String -> IO Fun
 makeZot "" = return empty
 makeZot bs = do
 	f <- makeZot $ init bs
 	f $$ case last bs of
-		'1'	-> one
-		'0'	-> zero
+		'1'	-> One -- one
+		'0'	-> Zero -- zero
 		_	-> error "makeZot: appear not '0' or '1'"
 
 pr :: Fun
-pr = Fun $ \x -> interrogate x $$$ Int 0 $$$ Int 1 >>= putStr . show >> return pr
+pr = Fun $ \x -> interrogate x >$$ Zero >$$ One >>= putStr . show >> return pr
 
 interrogate :: Fun -> IO Fun
-interrogate f = f $$ i $$$ i $$$ i $$$ k
+interrogate f = f $$ i >$$ i >$$ i >$$ k
 
 output :: IO Fun
-output = k $$$$ k $$$$ k $$$$ k $$$$ k $$$$ k $$ i
+output = k $$< k $$< k $$< k $$< k $$< k $$ i
