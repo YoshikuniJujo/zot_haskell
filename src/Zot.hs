79569249a0f6ca843972ebf11b805bdf4c7f231a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-13 13:55:53 +0900

    refactoring: Zot finished

--- src/Zot.hs
+++ src/Zot.hs
@@ -1,26 +1,37 @@
 module Zot ( main, mainFile ) where
 
-import Data.Char ( isSpace )
+import Control.Monad ( foldM )
 
 data Fun = Fun ( Fun -> IO Fun ) | Zero | One
 
 mkFun :: ( Fun -> Fun ) -> Fun
 mkFun f =  Fun $ return . f
 
-cont :: Fun -> Fun
-cont = Fun . flip ( $$ )
-
 instance Show Fun where
 	show Zero	= "0"
 	show One	= "1"
 	show ( Fun _ )	= "function"
+	showList fs	= ( concatMap show fs ++ )
+
+instance Read Fun where
+	readsPrec _ ( '0' : rest )	= [ ( Zero, rest ) ]
+	readsPrec _ ( '1' : rest )	= [ ( One, rest ) ]
+	readsPrec _ _			= error "read Fun failed"
+	readList str			= [ readl str ]
+		where
+		readl ""		= ( [ ], "" )
+		readl ( '0' : cs )	= let ( r, rest ) = readl cs in
+						( Zero : r, rest )
+		readl ( '1' : cs )	= let ( r, rest ) = readl cs in
+						( One : r, rest )
+		readl _			= error "read [ Fun ] failed"
 
 ( $$ ) :: Fun -> Fun -> IO Fun
 ( $$ ) ( Fun f )	= f
 ( $$ ) Zero		= ( zero $$ )
 ( $$ ) One		= ( one $$ )
 
-infixl 7 >$$
+infixl 6 >$$
 ( >$$ ) :: IO Fun -> Fun -> IO Fun
 f >$$ a = f >>= ( $$ a )
 
@@ -28,9 +39,12 @@ infixr 8 $$<
 ( $$< ) :: Fun -> IO Fun -> IO Fun
 f $$< a = ( f $$ ) =<< a
 
-infixl 6 >$$<
+infixl 7 >$$<
 ( >$$< ) :: IO Fun -> IO Fun -> IO Fun
 f >$$< a = f >>= ( $$< a )
+
+cont :: Fun -> Fun
+cont = Fun . flip ( $$ )
 
 s, k, i :: Fun
 s = mkFun $ \x -> mkFun $ \y -> Fun $ \z -> x $$ z >$$< y $$ z
@@ -42,15 +56,6 @@ empty	= cont i
 zero	= cont $ Fun $ \f -> f $$ s >$$ k
 one	= mkFun $ \c -> cont $ mkFun $ \l -> cont $ Fun $ \r -> c $$< l $$ r
 
-makeZot :: String -> IO Fun
-makeZot "" = return empty
-makeZot bs = do
-	f <- makeZot $ init bs
-	f $$ case last bs of
-		'1'	-> One
-		'0'	-> Zero
-		_	-> error "makeZot: appear not '0' nor '1'"
-
 pr :: Fun
 pr = Fun $ \x -> interrogate x >$$ Zero >$$ One >>= putStr . show >> return pr
 
@@ -60,18 +65,19 @@ interrogate f = f $$ i >$$ i >$$ i >$$ k
 output :: IO Fun
 output = k $$< k $$< k $$< k $$< k $$< k $$ i
 
-readPrint :: String -> IO ()
-readPrint src = do
-	fun <- makeZot $ filter ( not . isSpace ) $ remCom src
-	fun $$< output >$$ pr >> putStrLn ""
+readZot :: String -> IO Fun
+readZot = foldM ( $$ ) empty . read . filter ( `elem` "01" )
+
+interpret :: String -> IO ()
+interpret src = readZot ( removeComment src ) >$$< output >$$ pr >> putStrLn ""
 	where
-	remCom = unlines . map ( takeWhile ( /= '#' ) ) . lines
+	removeComment = concat . map ( takeWhile ( /= '#' ) ) . lines
 
 main :: IO ()
-main = getContents >>= readPrint
+main = getContents >>= interpret
 
 mainFile :: String -> IO ()
 mainFile fn = do
 	prg <- readFile fn
 	arg <- getContents
-	readPrint $ prg ++ arg
+	interpret $ prg ++ arg
