e8778a568de1d24293f63b9014b3b7bcd70e7a32
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-13 12:45:03 +0900

    refactoring: Zot not yet finished

--- src/Zot.hs
+++ src/Zot.hs
@@ -4,18 +4,21 @@ import Data.Char ( isSpace )
 
 data Fun = Fun ( Fun -> IO Fun ) | Zero | One
 
+mkFun :: ( Fun -> Fun ) -> Fun
+mkFun f =  Fun $ return . f
+
+cont :: Fun -> Fun
+cont = Fun . flip ( $$ )
+
 instance Show Fun where
 	show Zero	= "0"
 	show One	= "1"
 	show ( Fun _ )	= "function"
 
-apply :: Fun -> Fun -> IO Fun
-apply ( Fun f )	= f
-apply Zero	= apply zero
-apply One	= apply one
-
 ( $$ ) :: Fun -> Fun -> IO Fun
-( $$ ) = apply
+( $$ ) ( Fun f )	= f
+( $$ ) Zero		= ( zero $$ )
+( $$ ) One		= ( one $$ )
 
 infixl 7 >$$
 ( >$$ ) :: IO Fun -> Fun -> IO Fun
@@ -29,48 +32,24 @@ infixl 6 >$$<
 ( >$$< ) :: IO Fun -> IO Fun -> IO Fun
 f >$$< a = f >>= ( $$< a )
 
-retFun :: ( Fun -> IO Fun ) -> IO Fun
-retFun = return . Fun
-
-cont :: Fun -> Fun
-cont = Fun . flip ( $$ )
-
-retCont :: Fun -> IO Fun
-retCont = return . cont
-
 s, k, i :: Fun
-s = Fun $ \x -> retFun $ \y -> retFun $ \z -> x $$ z >$$< y $$ z
-k = Fun $ retFun . const . return
-i = Fun return
+s = mkFun $ \x -> mkFun $ \y -> Fun $ \z -> x $$ z >$$< y $$ z
+k = mkFun $ mkFun . const
+i = mkFun id
 
 empty, zero, one :: Fun
 empty	= cont i
 zero	= cont $ Fun $ \f -> f $$ s >$$ k
-one	= Fun $ \c -> retCont $	Fun $ \l -> retCont $ Fun $ \r -> c $$< l $$ r
-
-remCom :: String -> String
-remCom = unlines . map ( takeWhile ( /= '#' ) ) . lines
-
-main :: IO ()
-main = do
-	fun <- getContents >>= makeZot . filter ( not . isSpace ) . remCom
-	fun $$< output >$$ pr >> putStrLn ""
-
-mainFile :: String -> IO ()
-mainFile fn = do
-	prg <- readFile fn
-	arg <- getContents
-	fun <- makeZot $ filter ( not . isSpace ) $ remCom $ prg ++ arg
-	fun $$< output >$$ pr >> putStrLn ""
+one	= mkFun $ \c -> cont $ mkFun $ \l -> cont $ Fun $ \r -> c $$< l $$ r
 
 makeZot :: String -> IO Fun
 makeZot "" = return empty
 makeZot bs = do
 	f <- makeZot $ init bs
 	f $$ case last bs of
-		'1'	-> One -- one
-		'0'	-> Zero -- zero
-		_	-> error "makeZot: appear not '0' or '1'"
+		'1'	-> One
+		'0'	-> Zero
+		_	-> error "makeZot: appear not '0' nor '1'"
 
 pr :: Fun
 pr = Fun $ \x -> interrogate x >$$ Zero >$$ One >>= putStr . show >> return pr
@@ -80,3 +59,19 @@ interrogate f = f $$ i >$$ i >$$ i >$$ k
 
 output :: IO Fun
 output = k $$< k $$< k $$< k $$< k $$< k $$ i
+
+readPrint :: String -> IO ()
+readPrint src = do
+	fun <- makeZot $ filter ( not . isSpace ) $ remCom src
+	fun $$< output >$$ pr >> putStrLn ""
+	where
+	remCom = unlines . map ( takeWhile ( /= '#' ) ) . lines
+
+main :: IO ()
+main = getContents >>= readPrint
+
+mainFile :: String -> IO ()
+mainFile fn = do
+	prg <- readFile fn
+	arg <- getContents
+	readPrint $ prg ++ arg
