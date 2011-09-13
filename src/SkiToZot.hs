6c03443d4b8c2761d0270f465643d3b24709d774
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-13 14:48:04 +0900

    refactoring: SkiToZot and zotToSki finished

--- src/SkiToZot.hs
+++ src/SkiToZot.hs
@@ -4,10 +4,10 @@ main :: IO ()
 main = interact skiToZot
 
 skiToZot :: String -> String
-skiToZot "\n"		= ""
-skiToZot ""		= ""
-skiToZot ( '`' : rest )	= '1' : skiToZot rest
-skiToZot ( 'i' : rest ) = "100" ++ skiToZot rest
-skiToZot ( 'k' : rest ) = "1010100" ++ skiToZot rest
-skiToZot ( 's' : rest ) = "101010100" ++ skiToZot rest
-skiToZot _		= error "skiToZot error"
+skiToZot ""			= ""
+skiToZot ( '\n' : rest )	= skiToZot rest
+skiToZot ( '`' : rest )		= '1' : skiToZot rest
+skiToZot ( 'i' : rest ) 	= "100" ++ skiToZot rest
+skiToZot ( 'k' : rest ) 	= "1010100" ++ skiToZot rest
+skiToZot ( 's' : rest ) 	= "101010100" ++ skiToZot rest
+skiToZot _			= error "skiToZot error"
