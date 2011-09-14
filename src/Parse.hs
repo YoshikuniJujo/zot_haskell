a3ec7f57dbc9a51d270fd9b566fe00c4bfc7506b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-14 21:43:31 +0900

    refactoring: Parse not yet finished

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,33 +1,35 @@
 module Parse (
 	Parse,
 	none,
+	succeed,
 	spot,
+	token,
+	tokens,
+	eof,
 	(>*>),
 	alt,
 	build,
-	token,
-	tokens,
 	list1,
-	recL1,
-	eof
+	recL1
 ) where
 
-infixr 8 >*>
-infix 7 `build`
-infixl 6 `alt`
+import Control.Arrow ( ( &&& ) )
+
+infixr	8 >*>
+infix	7 `build`
+infixl	6 `alt`
 
 type Parse a b = [ a ] -> [ ( b, [ a ] ) ]
 
 none :: Parse a b
-none _ = [ ]
+none = const [ ]
 
 succeed :: b -> Parse a b
-succeed val inp = [ ( val, inp ) ]
+succeed = curry ( : [] )
 
 spot :: ( a -> Bool ) -> Parse a a
-spot p ( x : xs )
-	| p x		= [ ( x, xs ) ]
-spot _ _		= [ ]
+spot p ( x : xs )	| p x	= [ ( x, xs ) ]
+spot _ _			= [ ]
 
 token :: Eq a => a -> Parse a a
 token = spot . ( == )
@@ -36,12 +38,12 @@ tokens :: Eq a => [ a ] -> Parse a [ a ]
 tokens [ ]		= succeed [ ]
 tokens ( x : xs )	= token x >*> tokens xs `build` uncurry (:)
 
-alt :: Parse a b -> Parse a b -> Parse a b
-( p1 `alt` p2 ) inp = p1 inp ++ p2 inp
-
 (>*>) :: Parse a b -> Parse a c -> Parse a ( b, c )
 ( p1 >*> p2 ) inp =
 	[ ( ( y, z ), rem2 ) | ( y, rem1 ) <- p1 inp, ( z, rem2 ) <- p2 rem1 ]
+
+alt :: Parse a b -> Parse a b -> Parse a b
+p1 `alt` p2 = uncurry ( ++ ) . ( p1 &&& p2 )
 
 build :: Parse a b -> ( b -> c ) -> Parse a c
 build p f inp = [ ( f x, rem1 ) | ( x, rem1 ) <- p inp ]
