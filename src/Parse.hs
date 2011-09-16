22e7d45d49cb6c0c440f2b283370401b1c60e050
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-16 17:42:13 +0900

    refactoring: Parse finished

--- src/Parse.hs
+++ src/Parse.hs
@@ -13,7 +13,7 @@ module Parse (
 	recL1
 ) where
 
-import Control.Arrow ( ( &&& ) )
+import Control.Arrow ( first, ( &&& ) )
 
 infixr	8 >*>
 infix	7 `build`
@@ -38,26 +38,25 @@ tokens :: Eq a => [ a ] -> Parse a [ a ]
 tokens [ ]		= succeed [ ]
 tokens ( x : xs )	= token x >*> tokens xs `build` uncurry (:)
 
+eof :: Parse a ()
+eof [ ]	= [ ( (), [ ] ) ]
+eof _	= [ ]
+
 (>*>) :: Parse a b -> Parse a c -> Parse a ( b, c )
-( p1 >*> p2 ) inp =
-	[ ( ( y, z ), rem2 ) | ( y, rem1 ) <- p1 inp, ( z, rem2 ) <- p2 rem1 ]
+( p >*> q ) inp = [ ( ( x, y ), r' ) | ( x, r ) <- p inp, ( y, r' ) <- q r ]
 
 alt :: Parse a b -> Parse a b -> Parse a b
 p1 `alt` p2 = uncurry ( ++ ) . ( p1 &&& p2 )
 
 build :: Parse a b -> ( b -> c ) -> Parse a c
-build p f inp = [ ( f x, rem1 ) | ( x, rem1 ) <- p inp ]
+build p f = map ( first f ) . p
+
+list1 :: Parse a b -> Parse a [ b ]
+list1 p = p `build` ( : [] ) `alt` p >*> list1 p `build` uncurry ( : )
 
 recL1 :: ( b -> b -> b ) -> Parse a b -> Parse a b
 recL1 f p = ( p >*> recL1' f p ) `build` ( \( x, xs ) -> xs x )
 
 recL1' :: ( b -> c -> b ) -> Parse a c -> Parse a ( b -> b )
 recL1' f p = succeed id `alt`
-	( ( p >*> recL1' f p ) `build` ( \( x, xs ) y -> xs ( f y x ) ) )
-
-eof :: Parse a ()
-eof [ ]	= [ ( (), [ ] ) ]
-eof _	= [ ]
-
-list1 :: Parse a b -> Parse a [ b ]
-list1 p = p `build` ( : [] ) `alt` p >*> list1 p `build` uncurry ( : )
+	( p >*> recL1' f p `build` ( \( x, xs ) y -> xs ( f y x ) ) )
